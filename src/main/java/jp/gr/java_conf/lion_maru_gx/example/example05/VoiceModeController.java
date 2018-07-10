055b3bab4e0e9026ea0fc2f0f15f0eca82a80576
lion-maru-gx <lion-maru-gx@PC045.g-insight.co.jp>
2018-07-10 15:02:31 +0900

    VoiceMode画面

--- src/main/java/jp/gr/java_conf/lion_maru_gx/example/example05/VoiceModeController.java
+++ src/main/java/jp/gr/java_conf/lion_maru_gx/example/example05/VoiceModeController.java
@@ -3,7 +3,6 @@ package jp.gr.java_conf.lion_maru_gx.example.example05;
 import javafx.fxml.FXML;
 
 public class VoiceModeController extends Controller {
-
 	@FXML
     protected void handleNextButtonAction(){
         nextPage(App.Page.VoiceEdit);
